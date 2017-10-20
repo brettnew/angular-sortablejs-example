f04b56daf54d5b6fd166723cb481136d4e4a1cf0
Brett New <jumpingneptune@gmail.com>
2017-10-20 10:24:21 -0500

    Add angular-sortablejs package and references

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed, async } from '@angular/core/testing';
+import { SortablejsModule } from '@angular-sortablejs';
 
 import { AppComponent } from './app.component';
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SortablejsModule } from 'angular-sortablejs';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		SortablejsModule
+	],
+	exports: [SortablejsModule],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
